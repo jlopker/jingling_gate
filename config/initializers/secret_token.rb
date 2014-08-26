61e208608dd1046cdaccf1eb84f0af26fba9aa1a
cbestiva <cbestiva@gmail.com>
2014-08-26 11:17:56 -0700

    generated post model and styled views with bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JinglingBlog::Application.config.secret_key_base = 'e5852dec4d22681758c4bbe923ae1a2f7ff891698080719c4722dc2ce065a05d8147560c453a1cc979b524b48925856dc1ddbc357fc22c080c982ca4d79cb3c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JinglingBlog::Application.config.session_store :cookie_store, key: '_jingling_blog_session'
