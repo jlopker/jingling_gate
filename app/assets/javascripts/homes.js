8e76c796607dada02b18ef532aeac41cc7b6c6cf
Juliana <jlopker@gmail.com>
2014-10-03 14:26:30 -0700

    added scrolling spy, contact form

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,34 +1,98 @@
+$(window).load(function() {
+	init();
+})
+
 $(document).ready(function() {
-	// navigation click actions	
-	$('.scroll-link').on('click', function(event){
-		event.preventDefault();
-		var sectionID = $(this).attr("data-id");
-		scrollToID('#' + sectionID, 750);
-	});
-	// scroll to top action
-	$('.scroll-top').on('click', function(event) {
-		event.preventDefault();
-		$('html, body').animate({scrollTop:0}, 'slow'); 		
-	});
-	// mobile nav toggle
-	$('#nav-toggle').on('click', function (event) {
-		event.preventDefault();
-		$('#main-nav').toggleClass("open");
-	});
+	fullScreenContainer();
 });
-// scroll function
-function scrollToID(id, speed){
-	var offSet = 50;
-	var targetOffset = $(id).offset().top - offSet;
-	var mainNav = $('#main-nav');
-	$('html,body').animate({scrollTop:targetOffset}, speed);
-	if (mainNav.hasClass("open")) {
-		mainNav.css("height", "1px").removeClass("in").addClass("collapse");
-		mainNav.removeClass("open");
-	}
+
+function init() {
+	onePageScroll();
+	scrollAnchor();
 }
-if (typeof console === "undefined") {
-    console = {
-        log: function() { }
-    };
+
+/* --- Full Screen Container ------------- */
+
+function fullScreenContainer() {
+
+  // Set Initial Screen Dimensions
+
+  var screenWidth = $(window).width() + "px";
+  var screenHeight = $(window).height() + "px";
+
+  $("#home").css({
+    width: screenWidth,
+    height: screenHeight
+  });
+
+  // Every time the window is resized...
+
+  $(window).resize( function () {
+
+    // Fetch Screen Dimensions
+
+    var screenWidth = $(window).width() + "px";
+    var screenHeight = $(window).height() + "px";
+      
+    // Set Slides to new Screen Dimensions
+    
+    $("#home").css({
+      width: screenWidth,
+      height: screenHeight
+    }); 
+      
+  });
+
 }
+
+/* --- Scroll to Anchor ------------------- */
+
+function scrollAnchor() {
+
+  // scroll to specific anchor
+  $('.scroll').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 650);
+        return false;
+      }
+    }
+  });
+  
+}
+
+/* --- One Page Scroll ------------------- */
+
+function onePageScroll() {
+  $('.scrollnav').onePageNav({
+      currentClass: 'current',
+      changeHash: false,
+      scrollSpeed: 650,
+      scrollOffset: 30,
+      scrollThreshold: 0.5,
+      filter: ':not(.login, .signup)',
+      easing: 'swing',
+      begin: function() {
+          //I get fired when the animation is starting
+      },
+      end: function() {
+          //I get fired when the animation is ending
+      },
+      scrollChange: function($currentListItem) {
+          //I get fired when you enter a section and I pass the list item of the section
+      }
+  });
+}
+
+
+$(window).scroll(function() {
+  var windowpos = $(window).scrollTop() ;
+
+  if (windowpos <= 500) {
+      $('.nav li.current').removeClass('current');
+  }
+});
