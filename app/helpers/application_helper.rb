dbf69010792115377aff8b7321bdc574f646c5fe
Emma Lindsay <emma@flickme.com>
2015-02-11 13:22:00 -0800

    Randomize people listing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def people_array
+      [
+        {name: "Charlene Estiva",
+         title: "Web Developer",
+         picture: "charlene-avatar.png",
+         email: "charlene@jinglinggate.com",
+         bio: "Born and raised San Franciscan and General Assembly WDI alumna. Front-end developer interested in client-side technologies and UI. Self proclaimed golfer and bay area sports fan.",
+         linkedin: "https://www.linkedin.com/in/charleneestiva"},
+        {name: "Jason Bryant",
+         title: "iOS Developer",
+         picture: "jason-avatar.png",
+         email: "jasonbryant@stanfordalumni.org",
+         bio: "Stanford University EE graduate turned iOS and Web designer/developer. Ex-touring guitarist for Damian Marley, volunteer, and meditation enthusiast.",
+         linkedin: "https://www.linkedin.com/pub/jason-bryant/4b/a93/863"},
+        {name: "Emma Lindsay",
+         title: "Web Developer",
+         picture: "emma-avatar.jpg",
+         email: "emma@jinglinggate.com",
+         bio: "2007 MIT CS alumna who has been coding for over a decade. Took a year off to study massage therapy and is addicted to lattes.",
+         linkedin: "https://www.linkedin.com/in/ejlindsay"},
+        {name: "Juliana Lopker",
+         title: "Web Developer",
+         picture: "juliana-avatar.jpg",
+         email: "juliana@jinglinggate.com",
+         bio: "San Francisco n00b and Brown University alumna. Full stack developer specializing in the front-end and user experience. Lover of good design, dogs, and ice cream.",
+         linkedin: "https://www.linkedin.com/pub/juliana-lopker/36/871/942"}
+      ]
+  end
 end
